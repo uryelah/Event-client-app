0b082f44a677d4e41009b41f78b0dc2dadde140b
sarah <sarachamorro@gmail.com>
2020-03-27 20:01:02 -0300

    Fix nav, modal content and transition, types of events visual cues

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const Db = (() => ({
 }))();
 
 const User = (userObj) => {
-  const { firstName, lastName } = userObj;
+  const { firstName, lastName, picture } = userObj;
   const { premium, email } = userObj;
   const events = [];
   const id = Db.users.nextId;
@@ -40,6 +40,7 @@ const User = (userObj) => {
     id,
     firstName: capitalize(firstName),
     lastName: capitalize(lastName),
+    picture,
     premium,
     email,
     events,
@@ -108,24 +109,39 @@ const UserEvent = (joinObj) => {
 };
 
 const charly = User({
-  firstName: 'Charly', lastName: 'Adamns', premium: false, email: 'charchar@mail.com',
+  firstName: 'Charly', lastName: 'Adamns', premium: false, picture: 'https://www.shuttertalk.com/wp-content/uploads/2019/02/Photography-with-glasses.jpg', email: 'charchar@mail.com',
 });
 const winona = User({
-  firstName: 'Winona', lastName: 'Rider', premium: true, email: 'charchar@mail.com',
+  firstName: 'Winona', lastName: 'Rider', premium: true, picture: 'https://img-static.tradesy.com/item/24264036/ray-ban-multi-frame-and-demo-lens-women-rectangular-eyeglasses-1-1-960-960.jpg', email: 'charchar@mail.com',
 });
+
+const description = '      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quos inventore cum quia obcaecati dolore esse ratione dicta architecto hic provident, nam, impedit eum nisi quisquam veniam. Quibusdam, consequatur distinctio!';
 
 const recruitment = Event({
-  type: 'Leap', premium: false, title: 'Leap into', description: '...', country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 1, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
+  type: 'Leap', premium: false, title: 'Leap into', description, country: 'UK', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 1, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
 });
 const recruitment1 = Event({
-  type: 'past Leap', premium: false, title: 'Leap intoa', description: '...', country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 2, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
+  type: 'Mission', premium: false, title: 'Leap into', description, country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 2, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
 });
 
+Event({
+  type: 'Recruiting', premium: false, title: 'Leap into', description, country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 4, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
+});
+
+Event({
+  type: 'Meetup', premium: false, title: 'Leap into', description, country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 5, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
+});
+
+Event({
+  type: 'Webnar', premium: false, title: 'Leap into', description, country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 8, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
+});
+
+
 const mission = Event({
-  type: 'Mission', premium: true, title: 'Latin America', description: '...', country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 3, 21, 11, 0, 0, 0)), deadLine: 'Mon 1 2020',
+  type: 'Webnar', premium: true, title: 'Latin America', description, country: 'US', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 3, 21, 11, 0, 0, 0)), deadLine: 'Mon 1 2020',
 });
 const recruitment2 = Event({
-  type: 'Future Leap', premium: false, title: 'Leap into Canada', description: '...', country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 5, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
+  type: 'Vanhackathon', premium: false, title: 'Leap into Canada', description, country: 'CA', location: 'Sao Paulo', date: new Date(Date.UTC(2020, 5, 10, 15, 0, 0, 0)), deadLine: 'Mon 1 2020',
 });
 
 const closeModal = (cover, modal) => {
@@ -134,27 +150,37 @@ const closeModal = (cover, modal) => {
 };
 
 const attendingDetails = (key) => {
+  const eventContainer = document.getElementById('events');
   const {
-    title, premium, date, location, description,
+    country, type, title, premium, date, location, description, container, deadLine,
   } = Db.events[key];
   const modal = document.getElementById('event-modal');
-
   modal.querySelector('.modal-content').innerHTML = `
   <h2>${title}</h2>
-  <em>${premium ? '(Premium)' : ''}</em>
-  <br/>
-  <small>${date.toDateString()}</small>
-  <br/>
-  <small>${location}</small>
-  <p>${description}</p>
-  <br/>
-  <a class="twitter-share-button"
-  href="https://twitter.com/intent/tweet?text=Im%20going%20to%20the%20VanHack%20event%20at%20link"
-  data-size="large">
-Share on Twitter</a>
+  <div class='event-detail'>
+    ${premium ? '<p class="premium-flag">Premium-only</p>' : ''}
+    <p><strong>Event type: </strong> <span>${type}</span></p>
+    <p><strong>Country: </strong> <span>${country}</span></p>
+  </div>
+  <div class='event-description'>
+    <p>${description}</p>
+    <p><strong>Location: </strong> <span>${location}</span></p>
+    <p><strong>Date: </strong> <span>${date.toDateString()}</span></p>
+    ${Db.events[key].date.getTime() >= Date.now() ? `
+    <p><strong>Application deadline: </strong> ${deadLine}</p>
+    ` : ''}
+  </div>
+  <div class='social-media'>
+    <a class="twitter-share-button"
+    href="https://twitter.com/intent/tweet?text=Im%20going%20to%20the%20VanHack%20event%20at%20link"
+    data-size="large">
+    <i class="fab fa-twitter"></i></a>
+  </div>
   `;
 
+  modal.style.top = '0';
   modal.classList.remove('hidden');
+  modal.style.top = `${(document.body.clientHeight / 2) - (modal.clientHeight / 1.25)}px`;
   document.getElementById('doc-cover').classList.remove('hidden');
 };
 
@@ -172,19 +198,14 @@ const tempMessage = (element, message, type = 'warn') => {
 
 // / DOM STUFF
 window.onload = () => {
-  const userSelect = document.getElementById('user-select');
   const userGreet = document.getElementById('current-user-greet');
+  const userPic = document.getElementById('current-user-pic');
+  const changeUser = document.getElementById('change-user');
+  const hiddenSelect = document.getElementById('hidden-select');
   const eventContainer = document.getElementById('events');
   const applyedEvents = document.getElementById('my-events');
   const alertContainer = document.getElementById('alert');
   const pastEventContainer = document.getElementById('past-events');
-
-  for (const key in Db.users) {
-    const { id, firstName, premium } = Db.users[key];
-    if (key !== 'nextId') {
-      userSelect.innerHTML += `<option class='user' data-user=${id}>${firstName}(${premium ? 'premium' : 'regular'})</option>`;
-    }
-  }
 
   const populateEvents = (user) => {
     const events2Show = {
@@ -206,13 +227,13 @@ window.onload = () => {
     return events2Show;
   };
 
-  const eventComponent = (event, past = false) => {
+  const eventComponent = (event, past = false, attending = false) => {
     const {
       type, premium, id, title, date, location, deadLine, country,
     } = event;
     // MeetUp, Leap, Recruiting, Mission, VanHackathon, Premium-only Webnar, Open Webnar
     return (
-      `<article class='event ${past && 'card-micro'} card ${type}-event ${premium ? 'premium-event' : ''}' onclick='attendingDetails(${id})' data-event='${id}' id='${id}'>
+      `<article class='event ${past && !attending ? 'card-micro' : ''} ${attending ? 'card-macro' : ''} card ${type}-event ${premium ? 'premium-event' : ''}' onclick='attendingDetails(${id})' data-event='${id}' id='${id}'>
       <header>
         <h2 class='title'>${title}</h2>
         <figure class='flag-64'>
@@ -220,13 +241,30 @@ window.onload = () => {
         </figure>
       </header>
       <div class='card-cover ${type.toLowerCase()}-cover'>
+      ${premium
+        ? `<div class='premium-row'>
+        <i class='fa fa-star premium-star'></i>
+        <i class='fa fa-star premium-star-back'></i>
+        <span>Premium-only</span>
+      </div>`
+        : (['leap', 'mission', 'vanhackathon'].includes(type.toLowerCase()) ? `<div class='event-type'>
+          <div class='event-flag'>
+            ${type.toLowerCase() === 'leap' ? `<i class="fas fa-rainbow"></i>` : ''}
+
+            ${type.toLowerCase() === 'mission' ? `<i class="fas fa-users"></i>` : ''}
+
+            ${type.toLowerCase() === 'vanhackathon' ? `<i class="fas fa-laptop-code"></i>` : ''}
+          </div>
+          <span class='event-name'>${type}</span>
+        </div>` : '')}
       </div>
       <div class='card-info'>
         <time class='event-date' datetime='${date}'>${date.toDateString()}</time>
         <span><i class="fas fa-map-marker-alt"></i>${location}</span>
         ${!past ? `<span><i class="far fa-calendar-alt"></i> Deadline: <time datetime='${deadLine}'>${deadLine}</time></span>
         <div class='button-row'>
-          <button class='apply-btn button-default' data-event='${id}' type='button'>APPLY</button>
+          ${attending ? `<button class='aplication-btn button-default' data-event='${id}' type='button'>See Application</button>`
+        : `<button class='apply-btn button-default' data-event='${id}' type='button'>APPLY</button>` }
           <a class="twitter-share-button"
           href="https://twitter.com/intent/tweet?text=Check%20out%20this%20VanHack%20event%20at%20link"
           data-size="large">
@@ -238,6 +276,13 @@ window.onload = () => {
     );
   };
 
+  const emptyNoteComponent = (container) => {
+    if (container === 'applied') {
+      return `<p>You haven't applied to any event yet.</p>
+      <p>Find an event to attend below <i class="far fa-hand-point-down"></i></p>`
+    }
+  };
+
   const addEvent2Btns = () => {
     Array.from(document.getElementsByClassName('apply-btn')).forEach((btn) => {
       btn.addEventListener('click', (e) => {
@@ -245,16 +290,22 @@ window.onload = () => {
         const { logged } = loggedUser;
         const curUser = logged();
         if (!loggedUser.logged()) {
-          tempMessage(alertContainer, 'Login first to apply');
+          tempMessage(alertContainer, '<i class="fas fa-exclamation-triangle"></i> Login first to apply');
         } else {
           const newAttending = UserEvent({ userId: curUser.id, eventId: btn.dataset.event });
           if (newAttending === 'NOT_PREMIUM_ERROR') {
-            tempMessage(alertContainer, '<p>Sorry, this even is for premium users only. <a href="#">Learn more about premium memberships</a></p>');
+            tempMessage(alertContainer, '<i class="fas fa-exclamation-triangle"></i> Sorry, this even is for premium users only. <a href="#">Learn more about premium memberships</a>');
           } else if (newAttending !== 'ALREADY_APPLYED_ERROR') {
             logged().events.push(btn.dataset.event);
             document.getElementById(`${btn.dataset.event}`).remove();
-            applyedEvents.innerHTML += eventComponent(event, true);
-            tempMessage(alertContainer, `<p>You are now taking part at ${event.title}</p>`, 'success');
+
+            if (applyedEvents.classList.contains('events-empty')) {
+              applyedEvents.classList.remove('events-empty');
+              applyedEvents.innerHTML = '';
+            }
+
+            applyedEvents.innerHTML += eventComponent(event, false, true);
+            tempMessage(alertContainer, `<i class="fas fa-check-circle"></i> You are now taking part at ${event.title}`, 'success');
           }
         }
         e.stopPropagation();
@@ -283,8 +334,15 @@ window.onload = () => {
     });
 
     main.forEach((event) => {
-      event.container.innerHTML += eventComponent(event);
+      event.container.innerHTML += eventComponent(event, false, event.container === applyedEvents && true);
     });
+
+    if (applyedEvents.childElementCount === 0) {
+      applyedEvents.classList.add('events-empty');
+      applyedEvents.innerHTML = emptyNoteComponent('applied');
+    } else {
+      applyedEvents.classList.remove('events-empty');
+    }
   };
 
   const events = populateEvents(loggedUser.logged());
@@ -296,31 +354,62 @@ window.onload = () => {
 
   addEvent2Btns();
 
+  const populateUsers = () => {
+    for (const key in Db.users) {
+      const { id, firstName, premium } = Db.users[key];
+      if (key !== 'nextId') {
+        document.getElementById('user-select').innerHTML += `<option class='user' data-user=${id}>${firstName}(${premium ? 'premium' : 'regular'})</option>`;
+      }
+    }
+  };
+
+  const addEventToUserSelect = () => {
+    Array.from(document.getElementById('user-select').querySelectorAll('.user')).forEach((user) => {
+      user.addEventListener('click', () => {
+        const clickedUser = Db.users[user.dataset.user];
+        loggedUser.login(clickedUser);
+        checkLoggedUser();
+      });
+    });
+  };
+
   const checkLoggedUser = () => {
-    if (loggedUser.logged()) {
-      userGreet.innerHTML = `Welcome, ${loggedUser.logged().firstName}`;
-      const currentEvents = populateEvents(loggedUser.logged());
+    const user = loggedUser.logged();
+    if (user) {
+      userGreet.innerHTML = `Welcome, ${user.firstName}`;
+      userPic.innerHTML = `${user.premium ? '<i class="fa fa-star premium-pic-star"></i><div class="premium-user ' : '<div class="'}current-user-pic"><img class='profile-pic' src='${user.picture}' alt='profile-image'/></div>`;
+      hiddenSelect.innerHTML = `<select name="users" id="user-select">
+      <option class='user' data-user='-1' default>--</option>
+      </select>`;
+      changeUser.innerHTML = `<i class="fas fa-caret-down"></i>`;
+      const currentEvents = populateEvents(user);
       const sortedEvents = {
         main: sortEvents(currentEvents.main, 1),
         past: sortEvents(currentEvents.past, -1),
       };
+
       addEvents2Dom(sortedEvents);
       addEvent2Btns();
+      populateUsers();
+      addEventToUserSelect();
     } else {
-      userGreet.innerHTML = 'Login to use this app.';
-      applyedEvents.innerHTML = '';
+      userGreet.innerHTML = 'Hi, visitor';
+      userPic.innerHTML = '';
+      changeUser.innerHTML = '';
+      applyedEvents.innerHTML = `<article class='visitor-welcome'>
+        <h1>Welcome to the events app!</h1>
+        <p>Please, select one of the users below to login.</p>
+        <select name="users" id="user-select">
+        <option class='user' data-user='-1' default>--</option>
+        </select>
+      </article>`;
+      populateUsers();
     }
   };
 
   checkLoggedUser();
 
-  Array.from(userSelect.querySelectorAll('.user')).forEach((user) => {
-    user.addEventListener('click', () => {
-      const clickedUser = Db.users[user.dataset.user];
-      loggedUser.login(clickedUser);
-      checkLoggedUser();
-    });
-  });
+  addEventToUserSelect();
 
   addEvent2Btns();
 };
